1cda04cb271237f899d7d9aeeac9fbcd860205fd
Guilherme Rodrigues <48372755+CndGui@users.noreply.github.com>
2023-03-18 17:08:01 -0300

    Add files via upload
    + new navigation menu
    + new primary color

--- src/js/index.js
+++ src/js/index.js
@@ -1,55 +1,39 @@
+const sideBar = document.querySelector(".side-bar");
+const toggle = sideBar.querySelector(".toggle");
+const sideBarPhone = document.querySelector(".side-bar-phone")
+const togglePhone = sideBarPhone.querySelector(".toggle")
+const options = document.querySelectorAll(".menu .nav-link");
 const pages = document.querySelectorAll(".page");
-const homePage = pages[0];
-const aboutPage = pages[1];
-const myskillsPage = pages[2];
-const workPage = pages[3];
-const contactPage = pages[4];
 
-const options = Array.from(document.querySelectorAll(".option"));
-const home = document.querySelector(".logo")
-options.push(home)
+toggle.addEventListener("click", () => {
+    sideBar.classList.toggle("close");
+    sideBarPhone.classList.toggle("close");
+})
 
-selectOption()
+togglePhone.addEventListener("click", () => {
+    sideBar.classList.toggle("close");
+    sideBarPhone.classList.toggle("close");
+})
 
-function selectOption() {
+changePage()
+
+function changePage() {
     options.forEach(option => {
         option.addEventListener("click", () => {
-            const onOption = document.getElementsByClassName("option on");
-            if(onOption.length !== 0) onOption[0].classList.remove("on")
+            pages.forEach(page => {
+                const optionTarget = option.querySelector(".nav-text").innerHTML.toLowerCase().replace(/ /g, "-");
+                const pageTarget = page.classList[1]
 
-            if(option.classList.length == 1) {
-                if(option.classList[0] == "logo") {
-                    changePage(homePage);
+                page.classList.remove("on");
+
+                if(optionTarget == pageTarget) {
+                    page.classList.add("on")
+                    if (!sideBar.classList.contains("close")) {
+                        sideBar.classList.toggle("close");
+                        sideBarPhone.classList.toggle("close");
+                    }
                 }
-            }else {
-                if(option.classList[1] == "about") {
-                    changePage(aboutPage)
-
-                    option.classList.add("on")
-                }else if(option.classList[1] == "my-skills") {
-                    changePage(myskillsPage)
-
-                    option.classList.add("on")
-                }else if(option.classList[1] == "work") {
-                    changePage(workPage)
-
-                    option.classList.add("on")
-                }else if(option.classList[1] == "contact") {
-                    changePage(contactPage)
-
-                    option.classList.add("on")
-                }
-            }
+            })
         })
     })
-}
-
-function changePage(targetPage) {
-    pages.forEach(page => {
-        if(page.classList[page.classList.length -1] == "on") {
-            page.classList.remove("on");
-
-            targetPage.classList.add("on")
-        }
-    });
 }
